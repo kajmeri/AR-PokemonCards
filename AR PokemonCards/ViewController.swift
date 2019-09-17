5fc74e7f3944ceb9e7e186d2817dd1d03d487973
Krishna Ajmeri <krishnaajmeri93@gmail.com>
2019-09-16 22:47:03 -0400

    AR for one card, pops up model of pokemon on top of card

--- AR PokemonCards/ViewController.swift
+++ AR PokemonCards/ViewController.swift
@@ -11,65 +11,79 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+	
+	//MARK: - Variable Declaration
+	
+	@IBOutlet var sceneView: ARSCNView!
+	let node = SCNNode()
+	
+	//MARK: - View Methods
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		sceneView.delegate = self
+		sceneView.autoenablesDefaultLighting = true
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		// Create a session configuration
+		let configuration = ARImageTrackingConfiguration()
+		if let imagesToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
+			configuration.trackingImages = imagesToTrack
+			configuration.maximumNumberOfTrackedImages = 5
+			
+			print("Images successfully added")
+		}
+		
+		// Run the view's session
+		sceneView.session.run(configuration)
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		
+		// Pause the view's session
+		sceneView.session.pause()
+	}
+	
+	//MARK: - Touch/Motion Methods
+	
+	
+	//MARK: - Image Rendering Methods
+	
+	
+	
+	//MARK: - ARSCNViewDelegate Methods
+	
+	func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+		if let imageAnchor = anchor as? ARImageAnchor {
+			createPlane(withImageAnchor: imageAnchor)
+		}
+		
+		return node
+	}
+	
+	//MARK: - Plane Rendering Methods
+	
+	func createPlane(withImageAnchor imageAnchor: ARImageAnchor) {
+		let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+		let planeNode = SCNNode(geometry: plane)
+		
+		plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+		
+		planeNode.eulerAngles.x = -.pi / 2
+		
+		node.addChildNode(planeNode)
+		
+		if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn") {
+			if let pokeNode = pokeScene.rootNode.childNodes.first {
+				pokeNode.eulerAngles.x = .pi / 2
+				planeNode.addChildNode(pokeNode)
+			}
+			
+		}
+	}
 }
