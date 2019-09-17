3a94b102dae77c03b5e1ec516b27ac802f5f1b96
Krishna Ajmeri <krishnaajmeri93@gmail.com>
2019-09-16 23:48:09 -0400

    Added functionality for second pokemon card

--- AR PokemonCards/ViewController.swift
+++ AR PokemonCards/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 	//MARK: - Variable Declaration
 	
 	@IBOutlet var sceneView: ARSCNView!
-	let node = SCNNode()
 	
 	//MARK: - View Methods
 	
@@ -33,9 +32,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 		let configuration = ARImageTrackingConfiguration()
 		if let imagesToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
 			configuration.trackingImages = imagesToTrack
-			configuration.maximumNumberOfTrackedImages = 5
-			
-			print("Images successfully added")
+			configuration.maximumNumberOfTrackedImages = 2
 		}
 		
 		// Run the view's session
@@ -49,41 +46,50 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 		sceneView.session.pause()
 	}
 	
-	//MARK: - Touch/Motion Methods
-	
-	
-	//MARK: - Image Rendering Methods
-	
-	
-	
 	//MARK: - ARSCNViewDelegate Methods
 	
 	func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+		
+		let node = SCNNode()
+		
 		if let imageAnchor = anchor as? ARImageAnchor {
-			createPlane(withImageAnchor: imageAnchor)
+			
+			let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+			
+			plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+			
+			let planeNode = SCNNode(geometry: plane)
+			
+			planeNode.eulerAngles.x = -.pi / 2
+			
+			node.addChildNode(planeNode)
+			
+			if imageAnchor.referenceImage.name == "eevee-card" {
+				if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn") {
+					
+					if let pokeNode = pokeScene.rootNode.childNodes.first {
+						
+						pokeNode.eulerAngles.x = .pi / 2
+						
+						planeNode.addChildNode(pokeNode)
+					}
+				}
+			}
+			
+			if imageAnchor.referenceImage.name == "oddish-card" {
+				if let pokeScene = SCNScene(named: "art.scnassets/oddish.scn") {
+					
+					if let pokeNode = pokeScene.rootNode.childNodes.first {
+						
+						pokeNode.eulerAngles.x = .pi / 2
+						
+						planeNode.addChildNode(pokeNode)
+					}
+				}
+			}
 		}
 		
 		return node
-	}
-	
-	//MARK: - Plane Rendering Methods
-	
-	func createPlane(withImageAnchor imageAnchor: ARImageAnchor) {
-		let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
-		let planeNode = SCNNode(geometry: plane)
 		
-		plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
-		
-		planeNode.eulerAngles.x = -.pi / 2
-		
-		node.addChildNode(planeNode)
-		
-		if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn") {
-			if let pokeNode = pokeScene.rootNode.childNodes.first {
-				pokeNode.eulerAngles.x = .pi / 2
-				planeNode.addChildNode(pokeNode)
-			}
-			
-		}
 	}
 }
